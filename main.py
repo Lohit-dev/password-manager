e1220380931401d0378e2065070b3af955112771
Lohit <lohitsharma92@gmail.com>
2022-08-26 10:14:23 -0600

    fourth

--- main.py
+++ main.py
@@ -2,10 +2,31 @@ from cmath import log
 from email.mime import image
 from tkinter import *
 from tkinter import messagebox
-from turtle import bgcolor  # import tkinter
-
+from random import randint, choice, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+import random
+
+
+def password_gen():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for char in range(randint(8, 10))]
+    password_symbols = [choice(letters) for char in range(randint(2, 4))]
+    password_numbers = [choice(letters) for char in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -15,13 +36,14 @@ def save():
         email = email_entry.get()
         password = password_entry.get()
 
+        response = messagebox.askokcancel(title=website,
+                                          message=f"The details entered: \nEmail:{email}\n Password:{password}\nIs "
+                                                  f"it ok to save? ")
+        if response:
+            file.write(website + " " + email + " " + password + "\n")
 
-        messagebox.showinfo(message="Are you sure?")
-
-        file.write(website + " " + email + " " + password + "\n")
-
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -46,7 +68,7 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-generate_button = Button(text="Generate Password")
+generate_button = Button(text="Generate Password",command=password_gen)
 generate_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=36, command=save)
